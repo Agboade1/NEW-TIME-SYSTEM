c404894a29fb35dc395e39ff2fc0de9bcb456d3b
Agboade1 <ezekielakin20188@gmail.com>
2025-03-30 12:02:30 +0100

    Update defects.js

--- defects.js
+++ defects.js
@@ -1,10 +1,1 @@
-function submitDefect() {
-    let title = document.getElementById("defectTitle").value;
-    let description = document.getElementById("defectDescription").value;
-    
-    let defects = JSON.parse(localStorage.getItem("defects")) || [];
-    defects.push({ title, description });
-    localStorage.setItem("defects", JSON.stringify(defects));
 
-    alert("Defect reported successfully!");
-}
